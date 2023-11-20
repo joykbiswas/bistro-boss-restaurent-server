ff973bf5507871f18e2fd6e66850c4069ec872f3
joykbiswas <joykumarbiswas100@gmail.com>
2023-11-20 12:31:13 +0600

    payment details save and show from mongodb

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors =require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port =process.env.PORT || 5000;
 
 //middleware
@@ -32,6 +33,7 @@ async function run() {
     const menuCollection = client.db('bistroDB').collection('menu');
     const reviewCollection = client.db('bistroDB').collection('reviews');
     const cartCollection = client.db('bistroDB').collection('carts');
+    const paymentCollection = client.db('bistroDB').collection('payments');
 
     //jwt related api
     app.post('/jwt', async(req, res) =>{
@@ -185,6 +187,48 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result =await cartCollection.deleteOne(query)
+      res.send(result);
+    })
+
+    // payment intent
+    app.post('/create-payment-int', async(req, res) =>{
+      const {price} = req.body;
+      const amount = parseInt(price * 100)
+      console.log('amount inside', amount);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency: "usd",
+        payment_method_types: [
+          "card"
+        ],
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+    app.post('/payments', async (req, res) =>{
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // delete each item from the cart
+      console.log('payment info', payment);
+      const query = {_id: {
+        $in: payment.cartIds.map(id=>new ObjectId(id))
+      }}
+
+      const deleteResult = await cartCollection.deleteMany(query);
+    res.send({paymentResult, deleteResult});
+    })
+
+    // payment history show valid user 
+    app.get('/payments/:email',verifyToken, async(req, res) =>{
+      const query = { email: req.params.email}
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const result = await paymentCollection.find(query).toArray()
       res.send(result);
     })
 
